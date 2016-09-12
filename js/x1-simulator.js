17b941fe222831c346dc419d2641447b8c504182
Adina Halter <haltersweb@gmail.com>
2016-09-11 23:34:53 -0400

    fix retriggering overlay

--- js/x1-simulator.js
+++ js/x1-simulator.js
@@ -7,7 +7,7 @@
 (function ($) {
     'use strict';
     var tvInterface = $('.tv-interface'),
-    	modalTrigger = $('[aria-controls="youWin"]')[0];
+    	$modalTrigger = $($('[aria-controls="youWin"]')[0]);
     function ccSteps() {
     	var screens = tvInterface.find('img.cc'),
     		buttonSequence = [
@@ -15,23 +15,19 @@
     			NAME.remote.ok
     		],
     		n = 0;
-    	$(document).on('keydown', function(evt) {
+    	$(document).on('keydown.ccSteps', function(evt) {
     		console.log(evt.keyCode);
     		if (evt.keyCode === buttonSequence[n]) {
     			screens.hide();
     			screens.filter('.step-' + (n + 1).toString()).show();
     			n += 1;
-    		}
-    		if (n === buttonSequence.length) {
-
-                window.setTimeout(function() {
-                    console.log('click the button');
-                    modalTrigger.focus();
-                    modalTrigger.click();
-                }, 750);
-
-
-
+                if (n === buttonSequence.length) {
+                    window.setTimeout(function() {
+                        console.log('click the button');
+                        $modalTrigger.show().click();
+                        $('[data-trigger="true"]').removeAttr('data-trigger').hide();
+                    }, 750);
+                }
     		}
     	});
     }
